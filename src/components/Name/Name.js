df80aff2b172a43239cc7a02c5494597fc0c94ad
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-12-05 21:27:07 -0700

    first commit

--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, {UPDATE_NAME, UPDATE_CATEGORY} from './../../store.js'
 import "./Name.css";
+
 
 class Name extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      name: '',
-      category: ''
+      name: reduxState.name,
+      category: reduxState.category
     };
   }
   handleNameChange(nameVal) {
@@ -21,9 +24,19 @@ class Name extends Component {
       category: catVal
     });
   }
+
   saveChanges() {
     // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_NAME,
+      payload: this.state.name
+    });
+    store.dispatch({
+      type: UPDATE_CATEGORY,
+      payload: this.state.category
+    });
   }
+
   render() {
     return (
       <div className="Name forms">
--- src/store.js
+++ src/store.js
@@ -1,1 +1,33 @@
+import {createStore} from 'redux'
 
+const initialState = {
+    name: '',
+    category: '',
+    authorFirst: '',
+    authorLast: ''
+}
+
+export const UPDATE_NAME = 'UPDATE_NAME';
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
+export const UPDATE_AUTHOR_FIRST = 'UPDATE_AUTHOR_FIRST';
+export const UPDATE_AUTHOR_LAST = 'UPDATE_AUTHOR_LAST';
+
+
+
+function reducer(state = initialState, action){
+    const {type, payload} = action;
+    switch(type){
+        case UPDATE_NAME:
+            return {...state, name: payload}
+        case UPDATE_CATEGORY:
+            return {...state, category: payload}
+        case UPDATE_AUTHOR_FIRST:
+            return {...state, authorFirst: payload}
+        case UPDATE_CATEGORY:
+            return {...state, authorLast: payload}
+        default:
+            return state;
+    }
+}
+
+export default createStore(reducer);
